33f53060d60815822837fb30bd3d4d1921071a6e
riunosk <riunosk@outlook.com>
2020-11-11 13:45:18 +0800

    school

---
+++ Assets/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int moveSpeed;
+    public int rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+		{
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		}
+        if (Input.GetKey(KeyCode.S))
+		{
+            transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+		}
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -Time.deltaTime * rotateSpeed, 0));
+        }
+    }
+}
---
+++ Assets/Position.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Position : MonoBehaviour
+{
+    float X;
+    float Y;
+    float Z;
+
+    public GameObject PlayerPosition;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        X = Player.transform.position.x;
+        Y = Player.transform.position.y;
+        Z = Player.transform.position.z;
+        PlayerPosition.GetComponent<Text>().text = "Position: (" + X + ", " + Y + ", " + ", " + Z + ")";
+    }
+}
